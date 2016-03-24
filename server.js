4bc00b985ccc2c398ce2038715bc70299db0204b
Abuwabu <abuwabu@gmail.com>
2016-03-24 18:38:54 +0100

    Added custom message event

--- server.js
+++ server.js
@@ -22,8 +22,19 @@ app.use(express.static(__dirname + '/public'));
 
 
 // LISTEN UP
-io.on('connection', function() {
+io.on('connection', function(socket) {
   console.log("User connected via socket.io!");
+  
+  socket.on('message', function (message) {
+    console.log("Message received: " + message.text);
+    socket.broadcast.emit('message', message);
+  });
+  
+  // message is a custom name
+  // only get one argument — so use an object.
+  socket.emit('message', {
+    text: "Welcome to the Chat Application!"
+  });
 });
 
 http.listen(PORT, function(){
